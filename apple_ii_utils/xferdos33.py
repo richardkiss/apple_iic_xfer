9c82f55c2544ea53294730e4a8541a910f5ee869
Richard Kiss <him@richardkiss.com>
2023-07-29 17:18:13 -0700

    argparse

--- apple_ii_utils/xferdos33.py
+++ apple_ii_utils/xferdos33.py
@@ -1,87 +1,36 @@
-#!/usr/bin/env python3
-
 # about the Apple //c serial port:
 #   http://support.apple.com/kb/TA38851?viewlocale=en_US
 #   http://support.apple.com/kb/TA29788?viewlocale=en_US
 
-from functools import reduce
-from typing import Tuple
-
+import argparse
 import binascii
 import io
 import re
 import sys
 import time
+from functools import reduce
+from typing import Tuple
 
 import serial
 
-SERIAL_PORT_DEVICE = "/dev/tty.usbserial-10"
 
-APPLE_BIN = """8000:A9 0F 85 01 18 69 85 85
-8008:09 20 77 80 C6 09 C6 01
-8010:10 F7 A9 D4 A4 00 20 66
-8018:80 A9 85 85 09 A9 00 48
-8020:20 2E 80 68 A8 E6 09 C8
-8028:98 C0 10 90 F2 60 A0 00
-8030:84 04 84 02 84 03 A5 09
-8038:85 05 B1 04 48 20 DA FD
-8040:68 48 18 65 02 85 02 68
-8048:45 03 85 03 C8 98 29 1F
-8050:D0 03 20 72 80 98 D0 E2
-8058:A9 AB A4 02 20 66 80 A9
-8060:C5 A4 03 4C 66 80 20 ED
-8068:FD A9 BD 20 ED FD 98 20
-8070:DA FD A9 8D 4C ED FD 20
-8078:E3 03 84 07 85 08 A0 03
-8080:A9 00 91 07 A5 00 C8 91
-8088:07 A5 01 C8 91 07 A9 00
-8090:A0 08 91 07 A5 09 C8 91
-8098:07 A9 01 A0 0C 91 07 20
-80A0:E3 03 20 D9 03 A0 0D B1
-80A8:07 B0 01 60 4C 59 FF 00"""
-
-OUTPUT = sys.argv[1]
-
-s = """On the Apple, type:
-]IN#2
-
-then
-^A 15B (to set 19200 baud)
-^A 1D (to set 7N1)
-
-Note that ^A means "control-A".
-
-Then hit return. >"""
-
-t = input(s)
-
-p = serial.Serial(
-    SERIAL_PORT_DEVICE,
-    19200,
-    bytesize=serial.SEVENBITS,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    timeout=1,
-)
-
-
-def send_blind(t_str: str):
+def send_blind(port: serial.Serial, t_str: str):
     t = t_str.encode()
     for v in t:
         c = bytes([v])
-        p.write(c)
+        port.write(c)
         time.sleep(0.25)
 
 
-def send(t_str: str, wait_char: str = "]"):
+def send(port: serial.Serial, t_str: str, wait_char: str = "]"):
     t = t_str.encode()
     wait_char = wait_char.encode() if wait_char else None
 
     for v in t:
         c = bytes([v])
-        p.write(c)
+        port.write(c)
         while 1:
-            c1 = p.read(1)
+            c1 = port.read(1)
             same = c1 == c
             if c1 == b"\r":
                 c1 = b"\n"
@@ -92,7 +41,7 @@ def send(t_str: str, wait_char: str = "]"):
     if wait_char is None:
         return
     while 1:
-        c1 = p.read(1)
+        c1 = port.read(1)
         if c1 == b"\r":
             c1 = b"\n"
         sys.stdout.write(c1.decode())
@@ -101,15 +50,10 @@ def send(t_str: str, wait_char: str = "]"):
             break
 
 
-def send_lines(lines: str):
-    for t in lines.split("\n"):
-        send(t + "\r")
-
-
-def wait_for(sentinal: bytes) -> bytes:
+def wait_for(port: serial.Serial, sentinal: bytes) -> bytes:
     f = io.BytesIO()
     while 1:
-        c1 = p.read(1)
+        c1 = port.read(1)
         if c1 == b"\r":
             c1 = b"\n"
         f.write(c1)
@@ -134,39 +78,111 @@ def process_track(dump_bytes: bytes) -> Tuple[int, bytes]:
     track_idx = int(track_found[0], 16)
     sectors = SECTOR_RE.findall(dump)
     sector_data = []
-    for sector_idx, sector_tuple in enumerate(sectors):
+    for _sector_idx, sector_tuple in enumerate(sectors):
         d = b"".join(binascii.unhexlify(x) for x in sector_tuple[:8])
         actual_sum = sum(d) % 256
         actual_xor = reduce(lambda a, b: a ^ b, d, 0)
         expected_sum = int(sector_tuple[8], 16)
         expected_xor = int(sector_tuple[9], 16)
         if actual_sum != expected_sum or actual_xor != expected_xor:
-            raise Exception("bad checksum!! aborting")
+            msg = "bad checksum!! aborting"
+            raise OSError(msg)
         sector_data.append(d)
-        print(track_idx, sector_idx)
     track_data = b"".join(sector_data)
     return track_idx, track_data
 
 
+APPLE_BIN = """8000:A9 0F 85 01 18 69 85 85
+8008:09 20 77 80 C6 09 C6 01
+8010:10 F7 A9 D4 A4 00 20 66
+8018:80 A9 85 85 09 A9 00 48
+8020:20 2E 80 68 A8 E6 09 C8
+8028:98 C0 10 90 F2 60 A0 00
+8030:84 04 84 02 84 03 A5 09
+8038:85 05 B1 04 48 20 DA FD
+8040:68 48 18 65 02 85 02 68
+8048:45 03 85 03 C8 98 29 1F
+8050:D0 03 20 72 80 98 D0 E2
+8058:A9 AB A4 02 20 66 80 A9
+8060:C5 A4 03 4C 66 80 20 ED
+8068:FD A9 BD 20 ED FD 98 20
+8070:DA FD A9 8D 4C ED FD 20
+8078:E3 03 84 07 85 08 A0 03
+8080:A9 00 91 07 A5 00 C8 91
+8088:07 A5 01 C8 91 07 A9 00
+8090:A0 08 91 07 A5 09 C8 91
+8098:07 A9 01 A0 0C 91 07 20
+80A0:E3 03 20 D9 03 A0 0D B1
+80A8:07 B0 01 60 4C 59 FF 00"""
+
+
 def main():
-    send_blind("\r\3\rPR#2\r")
-    send("CALL-151\r", wait_char="*")
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description="Serial Port Data Logger")
+
+    # Add arguments
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=str,
+        default="/dev/tty.usbserial",
+        help="Serial port name (e.g., /dev/ttyUSB0, COM1)",
+    )
+
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        required=True,
+        help="Output file to save the data",
+    )
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    s = """On the Apple, type:
+]IN#2
+
+then
+^A 15B (to set 19200 baud)
+^A 1D (to set 7N1)
+
+Note that ^A means "control-A".
+
+Then hit return. >"""
+
+    t = input(s)
+
+    try:
+        port = serial.Serial(
+            args.port,
+            19200,
+            bytesize=serial.SEVENBITS,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            timeout=1,
+        )
+    except OSError:
+        parser.error(f"Can't open {args.port}")
+
+    send_blind(port, "\r\3\rPR#2\r")
+    send(port, "CALL-151\r", wait_char="*")
 
     for t in APPLE_BIN.split("\n"):
-        send(t + "\r", wait_char="*")
+        send(port, t + "\r", wait_char="*")
 
     track_data_list = []
 
     for track in range(35):
-        send("0:%02x\r" % track, wait_char="*")
-        send("8000G\r", wait_char=None)
-        track_dump = wait_for(b"*")
+        send(port, "0:%02x\r" % track, wait_char="*")
+        send(port, "8000G\r", wait_char=None)
+        track_dump = wait_for(port, b"*")
         actual_track, track_data = process_track(track_dump)
         track_data_list.append(track_data)
 
     disk_data = b"".join(track_data_list)
 
-    f = open(OUTPUT, "wb")
+    f = open(args.output, "wb")
     f.write(disk_data)
     f.close()
 
